0cd5bc88bc534ac3e6f370c1536a0298929fa797
Evan <evan.w.mclaughlin@gmail.com>
2022-07-01 10:27:11 -0400

    Update abnner

--- src/data/doc-it/Announcement.ts
+++ src/data/doc-it/Announcement.ts
@@ -10,10 +10,10 @@ type Announcement = {
 
 export const announcement: Announcement = {
     announcement: {
-        id: 1,
-        bannerContent: "We've related updated to <a href='/release-notes'> R24 </a>! Checkout our release presentation on <a href='https://www.youtube.com/watch?v=jFbutosoJ3Q' target='_blank'> Youtube </a>",
-        startDate: "2022-04-15",
-        endDate: "2022-04-25",
-        devOnly: true
+        id: 2,
+        bannerContent: "We've released updates for R25!  See our release notes for <a href='/release-notes'> details </a>.  ",
+        startDate: "2022-07-01",
+        endDate: "2022-07-14",
+        devOnly: false
     }
 };
