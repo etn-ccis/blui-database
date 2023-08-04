b4694d4f8d8fb71236b04c12657e8e2a95074acb
ektaghag-eaton <EktaNGhag@eaton.com>
2023-08-04 10:45:10 +0530

    fix theme related comments

--- src/data/doc-it/R29Roadmap.ts
+++ src/data/doc-it/R29Roadmap.ts
@@ -54,23 +54,23 @@ export const roadmap: RoadmapBucket[] = [
         ],
     },
     {
-        name: 'React Native Paper V5 with Material You',
-        description: 'Update React Native Component and Theme Library to support Paper V5 with Material You',
+        name: 'React Native Paper V5 with Material Design 3',
+        description: 'Update React Native Component Library and Themes to support Paper V5 with Material Design 3',
         type: 'development',
         framework: ['react-native'],
         items: [
             {
                 name: 'React Native Component Library',
                 description:
-                    'Update Component Library to support React Native Paper V5 with Material You',
+                    'Update Component Library to support React Native Paper V5 with Material Design 3',
                 year: CURRENT_YEAR,
                 quarter: CURRENT_QUARTER,
                 status: 'in-progress',
                 author: 'Brightlayer UI',
             },
             {
-                name: 'React Native Theme Library',
-                description: 'Update Theme Library to support React Native Paper V5 with Material You',
+                name: 'React Native Themes',
+                description: 'Update Themes to support React Native Paper V5 with Material Design 3',
                 year: CURRENT_YEAR,
                 quarter: CURRENT_QUARTER,
                 status: 'in-progress',
