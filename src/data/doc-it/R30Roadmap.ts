1d0f3ac4634da006300cc826f812ee6298cd8b1c
Suraj Karambe <surajakarambe@eaton.com>
2023-10-10 14:37:26 +0530

    Added blc integration and patch release in r30

--- src/data/doc-it/R30Roadmap.ts
+++ src/data/doc-it/R30Roadmap.ts
@@ -29,4 +29,38 @@ export const roadmap: RoadmapBucket[] = [
             },
         ],
     },
+    {
+        name: 'React Native Auth Workflow',
+        description: 'Brightlayer UI React Native Workflow',
+        type: 'development',
+        framework: ['react-native'],
+        items: [
+            {
+                name: 'React Native Auth Workflow patch release',
+                description:
+                    'Fix user-reported issues in the React Native Auth Workflow so that the adopters have robust and bug-free workflows',
+                year: CURRENT_YEAR,
+                quarter: CURRENT_QUARTER,
+                status: 'in-progress',
+                author: 'Brightlayer UI',
+            },
+        ],
+    },
+    {
+        name: 'API Integration',
+        description: 'Innersource Brightlayer Cloud integrations for React Auth Workflow',
+        type: 'development',
+        framework: ['react'],
+        items: [
+            {
+                name: 'Brightlayer Cloud API login/auth integrations',
+                description:
+                    'Innersource the BLC API login/auth integrations from DERMS CoE so that new products using React can connect their BLUI Auth workflow screens to BLC with minimal configuration',
+                year: CURRENT_YEAR,
+                quarter: CURRENT_QUARTER,
+                status: 'in-progress',
+                author: 'Brightlayer UI',
+            },
+        ],
+    },
 ];
