5712e38e327c7755abf74cff2abd309931b552cb
manojleaton <manojlokesh@eaton.com>
2024-01-22 13:22:36 +0530

    review comments incorporation

--- src/data/doc-it/R31Roadmap.ts
+++ src/data/doc-it/R31Roadmap.ts
@@ -5,8 +5,9 @@ const CURRENT_QUARTER: Quarter = "Q1";
 
 export const roadmap: RoadmapBucket[] = [
   {
-    name: "React Native Workflows",
-    description: "Brightlayer UI React Native Workflow",
+    name: "Reusable Workflows",
+    description:
+      "Components for implementing common workflows across applications",
     type: "development",
     framework: ["react-native"],
     items: [
@@ -18,42 +19,6 @@ export const roadmap: RoadmapBucket[] = [
         quarter: CURRENT_QUARTER,
         status: "in-progress",
         author: "Brightlayer UI",
-      },
-      {
-        name: "React Native Auth Workflow",
-        description:
-          "Develop a Flexible Architecture for React Native Auth Workflow",
-        year: CURRENT_YEAR,
-        quarter: CURRENT_QUARTER,
-        status: "in-progress",
-        author: "Brightlayer UI",
-      },
-      {
-        name: "React Native Auth Workflow patch release",
-        description:
-          "Fix adopter-reported issues in the React Native Auth Workflow",
-        year: CURRENT_YEAR,
-        quarter: CURRENT_QUARTER,
-        status: "in-progress",
-        author: "Brightlayer UI",
-      },
-    ],
-  },
-  {
-    name: "API Integration",
-    description:
-      "Innersource Brightlayer Cloud integrations for React Auth Workflow",
-    type: "development",
-    framework: ["react"],
-    items: [
-      {
-        name: "Brightlayer Cloud API auth integrations",
-        description:
-          "Innersource the Brightlayer Cloud API auth integrations from DERMS CoE so that new products using React can connect their Brightlayer UI Auth workflows to Brightlayer Cloud with minimal configuration",
-        year: CURRENT_YEAR,
-        quarter: CURRENT_QUARTER,
-        status: "in-progress",
-        author: "DERMS CoE",
       },
     ],
   },
