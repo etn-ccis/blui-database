25b2600d481e76a272f5eba911ffee63a3b40ad3
manojleaton <manojlokesh@eaton.com>
2024-01-22 12:28:23 +0530

    Updated R30 roadmap and added R31 roadmap

--- src/data/doc-it/R31Roadmap.ts
+++ src/data/doc-it/R31Roadmap.ts
@@ -1,42 +1,33 @@
 import { Quarter, RoadmapBucket } from "./_types";
 
-const CURRENT_YEAR = 2023;
-const CURRENT_QUARTER: Quarter = "Q4";
+const CURRENT_YEAR = 2024;
+const CURRENT_QUARTER: Quarter = "Q1";
 
 export const roadmap: RoadmapBucket[] = [
   {
-    name: "React Native Paper V5 with Material Design 3",
-    description:
-      "Update React Native Component Library and Themes to support Paper V5 with Material Design 3",
-    type: "development",
-    framework: ["react-native"],
-    items: [
-      {
-        name: "React Native Component Library",
-        description:
-          "Update Component Library to support React Native Paper V5 with Material Design 3",
-        year: CURRENT_YEAR,
-        quarter: CURRENT_QUARTER,
-        status: "finished",
-        author: "Brightlayer UI",
-      },
-      {
-        name: "React Native Themes",
-        description:
-          "Update Themes to support React Native Paper V5 with Material Design 3",
-        year: CURRENT_YEAR,
-        quarter: CURRENT_QUARTER,
-        status: "finished",
-        author: "Brightlayer UI",
-      },
-    ],
-  },
-  {
-    name: "React Native Auth Workflow",
+    name: "React Native Workflows",
     description: "Brightlayer UI React Native Workflow",
     type: "development",
     framework: ["react-native"],
     items: [
+      {
+        name: "React Native Registration Workflow",
+        description:
+          "Develop a Flexible Architecture for React Native Registration Workflow",
+        year: CURRENT_YEAR,
+        quarter: CURRENT_QUARTER,
+        status: "in-progress",
+        author: "Brightlayer UI",
+      },
+      {
+        name: "React Native Auth Workflow",
+        description:
+          "Develop a Flexible Architecture for React Native Auth Workflow",
+        year: CURRENT_YEAR,
+        quarter: CURRENT_QUARTER,
+        status: "in-progress",
+        author: "Brightlayer UI",
+      },
       {
         name: "React Native Auth Workflow patch release",
         description:
@@ -59,6 +50,23 @@ export const roadmap: RoadmapBucket[] = [
         name: "Brightlayer Cloud API auth integrations",
         description:
           "Innersource the Brightlayer Cloud API auth integrations from DERMS CoE so that new products using React can connect their Brightlayer UI Auth workflows to Brightlayer Cloud with minimal configuration",
+        year: CURRENT_YEAR,
+        quarter: CURRENT_QUARTER,
+        status: "in-progress",
+        author: "DERMS CoE",
+      },
+    ],
+  },
+  {
+    name: "Figma Components",
+    description: "Brightlayer Charts Figma Components",
+    type: "design",
+    framework: ["all"],
+    items: [
+      {
+        name: "Brightlayer UI Component Sticker Sheet",
+        description:
+          "Merge Brightlayer Charts to Brightlayer UI Component Sticker Sheet(Material Design 3)",
         year: CURRENT_YEAR,
         quarter: CURRENT_QUARTER,
         status: "in-progress",
